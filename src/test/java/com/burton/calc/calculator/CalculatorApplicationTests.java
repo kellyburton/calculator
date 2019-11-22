01098dad04e084ea56df0b81e48a72d619594d6e
Kelly Burton <kburton@localhost.localdomain>
2019-11-22 14:04:42 -0600

    Add Spring Boot Skeleton

---
+++ src/test/java/com/burton/calc/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burton.calc.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
